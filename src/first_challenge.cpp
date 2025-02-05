16a6434870a11e3e60149829b3ee0da5a890df97
Tsukoshi-Hiroki <ee213038@meiji.ac.jp>
2025-02-05 20:26:39 +0900

    refine-callback

--- src/first_challenge.cpp
+++ src/first_challenge.cpp
@@ -11,6 +11,7 @@ class FirstChallenge : public rclcpp::Node
 {
 public:
   FirstChallenge();
+  int hz_ = 10;
 
 private:
   void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
@@ -20,45 +21,63 @@ private:
   rclcpp::Publisher<roomba_500driver_meiji::msg::RoombaCtrl>::SharedPtr cmd_vel_pub_;
 
   bool can_move_ = false;
-  roomba_500driver_meiji::msg::RoombaCtrl cmd_vel;
+  roomba_500driver_meiji::msg::RoombaCtrl cmd_vel_;
 
 };
 
 FirstChallenge::FirstChallenge() : Node("first_challenge")
 {
-  // ctrl_pub_ = nh_.advertise<roomba_500driver_meiji::msg::RoombaCtrl>("/roomba/control", 1, true);
-  // odom_sub_ = nh_.subscribe("odom", 1, &FirstChallenge::odom_callback, this);
+  hz_ = this->declare_parameter<int>("hz", 10);  // hz_のデフォルト値を10に設定
+
   odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom", rclcpp::QoS(1).reliable(), std::bind(&FirstChallenge::odom_callback, this, std::placeholders::_1));
   cmd_vel_pub_ = this->create_publisher<roomba_500driver_meiji::msg::RoombaCtrl>("/roomba/control", rclcpp::QoS(1).reliable());
 }
 
 void FirstChallenge::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
-  // if(*msg != NULL)
-  // {
-  //   can_move_ = true;
-  // }
-  can_move_ = true;
+
+  RCLCPP_INFO(this->get_logger(),"odom_callback");
+  if(msg != NULL)
+  {
+    RCLCPP_INFO(this->get_logger(),"sub odom");
+    can_move_ = true;
+  }
+  else{
+    RCLCPP_INFO(this->get_logger(),"none sub odom");
+  }
+
   pub_cmd_vel(can_move_);
 }
 
 
 void FirstChallenge::pub_cmd_vel(bool can_move)
 {
+  RCLCPP_INFO(this->get_logger(),"determine_cmd_vel");
   if(can_move_)
   {
-    cmd_vel.velocity = 1;
-    cmd_vel_pub_->publish(cmd_vel);
+    RCLCPP_INFO(this->get_logger(),"pub_cmd_vel");
+
+    cmd_vel_.mode = 11;
+    cmd_vel_.cntl.linear.x = 0.1;
+    cmd_vel_.cntl.angular.z = 0;
+
+    cmd_vel_pub_->publish(cmd_vel_);
   }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
 
-  if(rclcpp::ok()){
-    std::shared_ptr<FirstChallenge> fchallenge = std::make_shared<FirstChallenge>();
+  std::shared_ptr<FirstChallenge> fchallenge = std::make_shared<FirstChallenge>();
+  rclcpp::Rate loop_rate(fchallenge->hz_);
+
+  while(rclcpp::ok())
+  {
     rclcpp::spin_some(fchallenge);
+    loop_rate.sleep();
   }
+  rclcpp::shutdown();
+
   return 0;
 }
